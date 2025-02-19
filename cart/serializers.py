14752e58169f956d9ae9f97bf683bf7aaefe0e40
RohanTekale <tekalerohan7@gmail.com>
2025-02-19 18:10:44 +0530

    commited latest changes

--- cart/serializers.py
+++ cart/serializers.py
@@ -3,7 +3,7 @@ from .models import CartItem
 
 class CartItemSerializer(serializers.ModelSerializer):
     product_name = serializers.ReadOnlyField(source='product.name')
-    placed_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S")
+    placed_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S", read_only=True)
 
     class Meta:
         model = CartItem
